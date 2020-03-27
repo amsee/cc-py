d38f5071420b5a3a18a72e22fbc2ffcf847fdb8d
Amy <amsee@users.noreply.github.com>
2020-03-27 19:27:27 -0400

    adding files

---
+++ syntax/script.py
@@ -0,0 +1,9 @@
+# python 2
+print "This is how you print in python 2."
+
+# python 3
+print("This is how you print in python 3.")
+
+# concatenating strings
+print "Hello " + "there" + " funny bunny."
+
