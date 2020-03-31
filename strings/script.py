bb54407745a63a881ccf2ff131eaa08a6bb59d37
Amy <amsee@users.noreply.github.com>
2020-03-31 14:02:13 -0400

    adding files

---
+++ strings/script.py
@@ -0,0 +1,53 @@
+# exercise 1
+
+
+# Assign your variables below, each on its own line!
+caesar = "Graham"
+praline = "John"
+viking = "Teresa"
+
+# Put your variables above this line
+
+print caesar
+print praline
+print viking
+
+
+# exercise 2
+
+
+# The string below is broken. Fix it using the escape backslash!
+
+'This isn\'t flying, this is falling with style!'
+
+
+# exercise 3
+
+
+"""
+The string "PYTHON" has six characters,
+numbered 0 to 5, as shown below:
+
++---+---+---+---+---+---+
+| P | Y | T | H | O | N |
++---+---+---+---+---+---+
+  0   1   2   3   4   5
+
+So if you wanted "Y", you could just type
+"PYTHON"[1] (always start counting from 0!)
+"""
+fifth_letter = "MONTY"[4]
+
+print fifth_letter
+
+
+# exercise 4
+
+
+parrot = "Norwegian Blue"
+print len(parrot)
+print parrot.lower()
+print parrot.upper()
+
+pi = 3.14
+print str(pi)
